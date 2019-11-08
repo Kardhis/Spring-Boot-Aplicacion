7edb478dbe14984919b7640a4db018d7b315b70e
David Castel <davidcastelcastells@gmail.com>
2019-11-08 00:33:54 +0000

    Commit inicial con Spring Tool Suite.

---
+++ SpringBootApplication/src/test/java/com/springBootAppication/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootAppication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
